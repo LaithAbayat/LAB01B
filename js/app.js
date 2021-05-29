8261f380ebcd6fc46a31c846d2adaaa9454cf2cf
LaithAbayat <laithabayat@gmail.com>
2021-05-29 17:42:12 +0300

    Edit lab01b

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let userName = prompt('Whats your name?');
+console.log(userName);
+alert("Nice to meet you");
+
+let userAge = prompt('Whats your age?');
+console.log(userAge);
+alert("Al3omor kloo :))");
+
+let favColor = prompt('Whats your favourite color?');
+console.log(favColor);
+alert("Good taste :)");
+
+
+let favSport = prompt('Whats your favourite sport?');
+console.log(favSport);
+alert("I like Soccor");
